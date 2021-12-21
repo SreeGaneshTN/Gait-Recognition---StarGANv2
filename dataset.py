8ecaea8d8e288e63672826f4627a36cbfa27c82d
SreeGaneshTN <tnsreeganesh@gmail.com>
2021-12-22 00:34:04 +0530

    Input fetcher added

---
+++ dataset.py
@@ -0,0 +1,65 @@
+
+import torch
+import torch.utils.data as data
+
+from itertools import chain
+from pathlib import Path
+import glob
+import os
+from munch import Munch
+import numpy as np
+import random
+from torch.utils.data.sampler import WeightedRandomSampler
+from PIL import Image
+
+
+def listdir(dname):
+    fnames = list(chain(*[list(Path(dname).rglob('*.' + ext))
+                          for ext in ['png', 'jpg', 'jpeg', 'JPG']]))
+    return fnames
+
+
+class InputFetcher:
+    def __init__(self, loader, loader_ref=None, latent_dim=16, mode=''):
+        self.loader = loader
+        self.loader_ref = loader_ref
+        self.latent_dim = latent_dim
+        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+        self.mode = mode
+
+    def _fetch_inputs(self):
+        try:
+            x, y = next(self.iter)
+        except (AttributeError, StopIteration):
+            self.iter = iter(self.loader)
+            x, y = next(self.iter)
+        return x, y
+
+    def _fetch_refs(self):
+        try:
+            x, x2, y = next(self.iter_ref)
+        except (AttributeError, StopIteration):
+            self.iter_ref = iter(self.loader_ref)
+            x, x2, y = next(self.iter_ref)
+        return x, x2, y
+
+    def __next__(self):
+        x, y = self._fetch_inputs()
+        if self.mode == 'train':
+            x_ref, x_ref2, y_ref = self._fetch_refs()
+            z_trg = torch.randn(x.size(0), self.latent_dim)
+            z_trg2 = torch.randn(x.size(0), self.latent_dim)
+            inputs = Munch(x_src=x, y_src=y, y_ref=y_ref,
+                           x_ref=x_ref, x_ref2=x_ref2,
+                           z_trg=z_trg, z_trg2=z_trg2)
+        elif self.mode == 'val':
+            x_ref, y_ref = self._fetch_inputs()
+            inputs = Munch(x_src=x, y_src=y,
+                           x_ref=x_ref, y_ref=y_ref)
+        elif self.mode == 'test':
+            inputs = Munch(x=x, y=y)
+        else:
+            raise NotImplementedError
+
+        return Munch({k: v.to(self.device)
+                      for k, v in inputs.items()})
